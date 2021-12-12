480b125d460720d0d03aaebf213451f21e4176e3
Vitor Paholsky <vitor.duarte@fulltechpro.com.br>
2021-12-12 19:24:17 -0300

    Adicionar arquivos de projeto.

---
+++ Todo.Domain/Commands/Contracts/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flunt.Validations;
+
+namespace Todo.Domain.Commands.Contracts
+{
+    public interface ICommand : IValidatable
+    {
+        
+    }
+}
---
+++ Todo.Domain/Enttities/TodoItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Domain.Enttities
+{
+    public class TodoItem : Entity
+    {
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+        public string  User { get; private set; }
+
+        public TodoItem(string title, bool done, DateTime date, string user)
+        {
+            Title = title;
+            Done = done;
+            Date = date;
+            User = user;
+        }
+
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+
+        public void MarkAsUndone()
+        {
+            Done = false;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+    }
+}

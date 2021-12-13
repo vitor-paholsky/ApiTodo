0596257e1e928775f1a2e3602de5e6aa05ec88ad
Vitor Paholsky <vitor.duarte@fulltechpro.com.br>
2021-12-12 22:41:18 -0300

    Handle

--- Todo.Domain/Commands/GenericCommandResult.cs
+++ Todo.Domain/Commands/GenericCommandResult.cs
@@ -9,5 +9,19 @@ namespace Todo.Domain.Commands
 {
     public class GenericCommandResult : ICommandResult
     {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+
+        public GenericCommandResult(bool success, string message, object data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public GenericCommandResult()
+        {
+        }
     }
 }
---
+++ Todo.Domain/Handlers/Contracts/IHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Handlers.Contracts
+{
+    public interface IHandler
+    {
+        public interface IHandler<T> where T : ICommand
+        {
+            ICommandResult Handle(T command);
+        }
+    }
+}

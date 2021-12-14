caa3790603dd69f569add7c51641b2d412852a05
Vitor Paholsky <vitor.duarte@fulltechpro.com.br>
2021-12-14 15:33:45 -0300

    Domain tests

---
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.Commands;
+
+namespace Todo.Domain.Tests.CommandTests;
+
+[TestClass]
+public class CreateTodoCommandTests
+{
+    private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("","", DateTime.Now);
+    private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Título tarefa","Vitor Paholsky", DateTime.Now);
+
+    public CreateTodoCommandTests()
+    {
+        _invalidCommand.Validate();
+        _validCommand.Validate();
+    }
+
+    [TestMethod]
+    public void Dado_um_comando_invalido()
+    {
+        Assert.AreEqual(_invalidCommand.Valid, false);
+    }
+
+    [TestMethod]
+    public void Dado_um_comando_valido()
+    {
+        Assert.AreEqual(_validCommand.Valid, true);
+    }
+}
---
+++ Todo.Domain.Tests/CommandTests/MarkTodoAsDoneCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.Commands;
+
+namespace Todo.Domain.Tests.CommandTests;
+
+[TestClass]
+    public class MarkTodoAsDoneCommandTests
+    {    
+        private readonly MarkTodoAsDoneCommand _invalidCommand = new MarkTodoAsDoneCommand(default(Guid), "vitor paholsky");
+
+        public MarkTodoAsDoneCommandTests()
+        {
+            _invalidCommand.Validate();
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_invalido()
+        {
+            Assert.AreEqual(_invalidCommand.Valid, true);
+        }
+}

ba79d93b5889512ee5cd1a02cf2d46958c4f8890
Vitor Paholsky <vitor.duarte@fulltechpro.com.br>
2021-12-15 23:36:41 -0300

    Handler Tests

---
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Título da tarefa", "Vitor Paholsky", DateTime.Now);
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+        private GenericCommandResult _result = new GenericCommandResult();
+
+        public CreateTodoHandlerTests()
+        {
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_invalido_deve_interromper_a_execucao()
+        {
+            _result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(_result.Success, false);
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido_deve_criar_a_tarefa()
+        {
+            _result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(_result.Success, true);
+        }
+    }
+}
---
+++ Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Enttities;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Tests.Repositories
+{
+    public class FakeTodoRepository : ITodoRepository
+    {
+        public void Create(TodoItem todo)
+        {            
+        }
+
+        public void Update(TodoItem todo)
+        {            
+        }
+    }
+}
